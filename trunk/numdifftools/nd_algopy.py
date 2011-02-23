Fixed a bug + small cosmetic fixes in Hessian.
git-svn-id: http://numdifftools.googlecode.com/svn@17 81f426d6-4404-11de-9c03-5d6cfad8e937

trunk/numdifftools/nd_algopy.py
@@ -21,8 +21,12 @@ class _Common(object):
             self.x = x.copy()
             # STEP 1: trace the function evaluation
             cg = algopy.CGraph()
-            #x = np.array([algopy.Function(x[i]) for i in range(len(x))])
-            x = algopy.Function(x)
+            if True:
+                x = algopy.Function(x)
+                #x = [x]
+            else:
+                x = np.array([algopy.Function(x[i]) for i in range(len(x))])
+            
             y = self.fun(x)
             cg.trace_off()
             cg.independentFunctionList = [x]

trunk/numdifftools/speed_comparison/run_benchmarks.py
@@ -27,7 +27,7 @@ for N in gradient_N_list:
     f0 = f(3*np.ones(N))
     t = time.time();  gradient = algopy.Gradient(f, x=np.ones(N), method='forward'); preproc_time = time.time() - t
     t = time.time();  ref_g =  gradient(3*np.ones(N));  run_time = time.time() - t
-    results_gradient[method['algopy_reverse']] = run_time,  0, preproc_time
+    results_gradient[method['algopy_reverse']] = run_time,  0.0, preproc_time
         
 
     # scientifc
@@ -54,7 +54,7 @@ for N in gradient_N_list:
     
     results_gradient_list.append(results_gradient)
 
-results_gradients = np.array(results_gradient_list)
+results_gradients = np.array(results_gradient_list)+1e-18
 print 'results_gradients=\n',results_gradients
 
 # HESSIAN COMPUTATION
@@ -71,7 +71,7 @@ for N in hessian_N_list:
     f = benchmark1.F(N)
     t = time.time();  hessian = algopy.Hessian(f, np.ones(N), method='forward'); preproc_time = time.time() - t
     t = time.time();  ref_H = hessian(3*np.ones(N));  run_time = time.time() - t
-    results_hessian[method['algopy_reverse']] = run_time, 0, preproc_time    
+    results_hessian[method['algopy_reverse']] = run_time, 0.0, preproc_time    
     
 #    
     # Scientific
@@ -96,7 +96,7 @@ for N in hessian_N_list:
     results_hessian_list.append(results_hessian)
     
 
-results_hessians = np.array(results_hessian_list)
+results_hessians = np.array(results_hessian_list)+1e-18
 
 print hessian_N_list
 print 'results_hessians=\n',results_hessians
@@ -120,7 +120,7 @@ plotfun(gradient_N_list, results_gradients[:,method['numdifftools'],0], '--ks', 
 pyplot.ylabel('time $t$ [seconds]')
 pyplot.xlabel('problem size $N$')
 pyplot.grid()
-leg = pyplot.legend(loc=2)
+leg = pyplot.legend(loc=0)
 frame= leg.get_frame()
 frame.set_alpha(0.4)
 pyplot.savefig('gradient_runtimes.png',format='png')
@@ -135,7 +135,7 @@ plotfun(hessian_N_list, results_hessians[:,method['numdifftools'],0], '--ks', ma
 pyplot.ylabel('time $t$ [seconds]')
 pyplot.xlabel('problem size $N$')
 pyplot.grid()
-leg = pyplot.legend(loc=2)
+leg = pyplot.legend(loc=0)
 frame= leg.get_frame()
 frame.set_alpha(0.4)
 pyplot.savefig('hessian_runtimes.png',format='png')
@@ -152,7 +152,7 @@ plotfun(gradient_N_list, results_gradients[:,method['algopy_forward'],2], '-.k>'
 pyplot.ylabel('time $t$ [seconds]')
 pyplot.xlabel('problem size $N$')
 pyplot.grid()
-leg = pyplot.legend(loc=2)
+leg = pyplot.legend(loc=0)
 frame= leg.get_frame()
 frame.set_alpha(0.4)
 pyplot.savefig('gradient_preprocessingtimes.png',format='png')
@@ -167,7 +167,7 @@ plotfun(hessian_N_list, results_hessians[:,method['numdifftools'],2], '--ks', ma
 pyplot.ylabel('time $t$ [seconds]')
 pyplot.xlabel('problem size $N$')
 pyplot.grid()
-leg = pyplot.legend(loc=2)
+leg = pyplot.legend(loc=0)
 frame= leg.get_frame()
 frame.set_alpha(0.4)
 pyplot.savefig('hessian_preprocessingtimes.png',format='png')
@@ -182,7 +182,7 @@ plotfun(gradient_N_list, results_gradients[:,method['algopy_forward'],1], '-.k>'
 pyplot.ylabel(r'relative error $\|g_{ref} - g\|/\|g_{ref}\}$')
 pyplot.xlabel('problem size $N$')
 pyplot.grid()
-leg = pyplot.legend(loc=2)
+leg = pyplot.legend(loc=0)
 frame= leg.get_frame()
 frame.set_alpha(0.4)
 pyplot.savefig('gradient_errors.png',format='png')
@@ -197,7 +197,7 @@ plotfun(hessian_N_list, results_hessians[:,method['algopy_reverse'],1], '-.k<', 
 pyplot.ylabel(r'relative error $\|H_{ref} - H\|/\|H_{ref}\|$')
 pyplot.xlabel(r'problem size $N$')
 pyplot.grid()
-leg = pyplot.legend(loc=2)
+leg = pyplot.legend(loc=0)
 frame= leg.get_frame()
 frame.set_alpha(0.4)
 pyplot.savefig('hessian_preprocessingtimes.png',format='png')

trunk/numdifftools/test/test_hessian.py
@@ -1,8 +1,6 @@
 # -*- coding:utf-8 -*-
 """
-Created on 6. feb. 2011
 
-@author: pab
 """
 
 # import a tool to use / as a symbol for normal division
@@ -31,52 +29,55 @@ class classicalHamiltonian:
        
         self.N = 2                            #N is a scalar, it's the number of ions in the chain
         f = 1000000                            #f is a scalar, it's the trap frequency
-        self.w = 2*pi*f                         #w is a scalar, it's the angular velocity corresponding to the trap frequency
-        self.C = (4*pi*constants.epsilon_0)**(-1)*constants.e**2    #C is a scalar, it's the Coulomb constant times the electronic charge in SI
-        self.m = 39.96*1.66e-27                        #m is the mass of a single trapped ion in the chain
+        self.w = 2 * pi * f                         #w is a scalar, it's the angular velocity corresponding to the trap frequency
+        self.C = (4 * pi * constants.epsilon_0) ** (-1) * constants.e ** 2    #C is a scalar, it's the Coulomb constant times the electronic charge in SI
+        self.m = 39.96 * 1.66e-27                        #m is the mass of a single trapped ion in the chain
        
        
 
 
     def potential(self, positionvector):                     #Defines the potential that is going to be minimized
        
-        x= positionvector                         #x is an 1-d array (vector) of lenght N that contains the positions of the N ions
-        w= self.w
-        C= self.C
-        m= self.m
+        x = positionvector                         #x is an 1-d array (vector) of lenght N that contains the positions of the N ions
+        w = self.w
+        C = self.C
+        m = self.m
        
        
         #First we consider the potential of the harmonic osszilator
-        Vx = 0.5 * m * (w**2) * sum(x**2)               
+        Vx = 0.5 * m * (w ** 2) * sum(x ** 2)               
        
         for i in range(len(x)):
-            for j in range(i+1, len(x)):
-                Vx += C /(abs(x[i]-x[j]))    #then we add the coulomb interaction
+            for j in range(i + 1, len(x)):
+                Vx += C / (abs(x[i] - x[j]))    #then we add the coulomb interaction
                
         return Vx
     
     def initialposition(self):        #Defines the initial position as an estimate for the minimize process
        
-        N= self.N
-        x_0 = r_[-(N-1)/2:(N-1)/2:N*1j]
+        N = self.N
+        x_0 = r_[-(N - 1) / 2:(N - 1) / 2:N * 1j]
         return x_0
        
     def normal_modes(self, eigenvalues):    #the computed eigenvalues of the matrix Vx are of the form (normal_modes)2*m.
         m = self.m
-        normal_modes = sqrt(eigenvalues/m)
+        normal_modes = sqrt(eigenvalues / m)
         return normal_modes
 
 def main():
     #C=(4*pi*constants.epsilon_0)**(-1)*constants.e**2
-    c=classicalHamiltonian()
-    #print c.potential(array([-0.5, 0.5]))
-    xopt = optimize.fmin(c.potential, c.initialposition(), xtol = 1e-10)
-    hessian = nd.Hessian(c.potential, stepMax=1e-1)
+    c = classicalHamiltonian()
+    print c.potential(array([-0.5, 0.5]))
+    print c.potential(array([-0.5, 0.0]))
+    print c.potential(array([0.0, 0.0]))
+    
+    xopt = optimize.fmin(c.potential, c.initialposition(), xtol=1e-10)
+    hessian = nd.Hessian(c.potential, stepMax=1.0, stepNom=np.abs(xopt))
     H = hessian(xopt)
     hessian.error_estimate
 
     eigenvalues = linalg.eigvals(H)
     normal_modes = c.normal_modes(eigenvalues)
 
-if __name__=='__main__':
+if __name__ == '__main__':
     main()

trunk/numdifftools/test/testnumdifftools.py
@@ -19,75 +19,75 @@ class TestDerivative(unittest.TestCase):
     def testderivative(self):
         #derivative of exp(x), at x == 0
         dexp = nd.Derivative(np.exp)
-        self.assertAlmostEqual(dexp(0),np.exp(0))
+        self.assertAlmostEqual(dexp(0), np.exp(0))
         dexp.derOrder = 2
-        self.assertAlmostEqual(dexp(0),np.exp(0))
+        self.assertAlmostEqual(dexp(0), np.exp(0))
 
         # Evaluate the indicated (default = first)
         # derivative at multiple points
         dsin = nd.Derivative(np.sin)
-        x = np.linspace(0,2.*np.pi,13)
+        x = np.linspace(0, 2. * np.pi, 13)
         y = dsin(x)
-        small = np.abs(y-np.cos(x))<dsin.error_estimate*10
+        small = np.abs(y - np.cos(x)) < dsin.error_estimate * 10
         self.assertTrue(np.all(small))
 
         #Higher order derivatives (second derivative)
         # Truth: 0
-        d2sin = nd.Derivative(np.sin,derOrder=2,stepFix=0.5)
+        d2sin = nd.Derivative(np.sin, derOrder=2, stepFix=0.5)
 
-        self.assertAlmostEqual(d2sin(np.pi),0.0,)
+        self.assertAlmostEqual(d2sin(np.pi), 0.0,)
 
         # Higher order derivatives (up to the fourth derivative)
         # Truth: sqrt(2)/2 = 0.707106781186548
         d2sin.derOrder = 4
-        y = d2sin(np.pi/4)
-        small = np.abs(y-np.sqrt(2.)/2.)<d2sin.error_estimate
+        y = d2sin(np.pi / 4)
+        small = np.abs(y - np.sqrt(2.) / 2.) < d2sin.error_estimate
         self.assertTrue(small)
 
         # Higher order derivatives (third derivative)
         # Truth: 1
-        d3cos = nd.Derivative(np.cos,derOrder=3)
-        y = d3cos(np.pi/2.0)
-        small = np.abs(y-1.0)< d3cos.error_estimate
+        d3cos = nd.Derivative(np.cos, derOrder=3)
+        y = d3cos(np.pi / 2.0)
+        small = np.abs(y - 1.0) < d3cos.error_estimate
         self.assertTrue(small)
 
         # Compute the derivative of a function using a backward difference scheme
         # And a backward scheme will only look below x0.
-        dsinh = nd.Derivative(np.sinh,method='backward')
-        small = np.abs(dsinh(0.0)-np.cosh(0.0))< dsinh.error_estimate
+        dsinh = nd.Derivative(np.sinh, method='backward')
+        small = np.abs(dsinh(0.0) - np.cosh(0.0)) < dsinh.error_estimate
         self.assertTrue(small)
 
         # Although a central rule may put some samples in the wrong places, it may still succeed
-        dlog = nd.Derivative(np.log,method='central')
+        dlog = nd.Derivative(np.log, method='central')
         x = 0.001
-        small = np.abs(dlog(x)-1.0/x)<dlog.error_estimate
+        small = np.abs(dlog(x) - 1.0 / x) < dlog.error_estimate
         self.assertTrue(small)
 
         #But forcing the use of a one-sided rule may be smart anyway
         dlog.method = 'forward'
-        small = np.abs(dlog(x)-1/x)<dlog.error_estimate
+        small = np.abs(dlog(x) - 1 / x) < dlog.error_estimate
         self.assertTrue(small)
 
         # Control the behavior of Derivative - forward 2nd order method, with only 1 Romberg term
         # Compute the first derivative, also return the final stepsize chosen
         #[deriv,err,fdelta] = derivest(@(x) tan(x),pi,'deriv',1,'Style','for','MethodOrder',2,'RombergTerms',1)
-        dtan = nd.Derivative(np.tan,derOrder=1,metOrder=2,method='forward',numTerms=1)
+        dtan = nd.Derivative(np.tan, derOrder=1, metOrder=2, method='forward', numTerms=1)
         y = dtan(np.pi)
         abserr = dtan.error_estimate
-        self.assertTrue(np.abs(y-1)<abserr)
+        self.assertTrue(np.abs(y - 1) < abserr)
 
         dtan.finaldelta
     
         ##%% Specify the step size (default stepsize = 0.1)
-        p0 = np.poly1d(range(1,6))
-        fd = nd.Derivative(p0,derOrder=4,stepFix=1.)
+        p0 = np.poly1d(range(1, 6))
+        fd = nd.Derivative(p0, derOrder=4, stepFix=1.)
         p4 = p0.deriv(4)
-        self.assertAlmostEqual(fd(1),p4(1))
+        self.assertAlmostEqual(fd(1), p4(1))
         
         ##%% Control the behavior of DERIVEST - forward 2nd order method, with only 1 Romberg term
         ##% Compute the first derivative, also return the final stepsize chosen
-        dtan = nd.Derivative(np.tan,derOrder=1,method='forward',metOrder=2,numTerms=1)
-        self.assertAlmostEqual(dtan(np.pi),1.0)
+        dtan = nd.Derivative(np.tan, derOrder=1, method='forward', metOrder=2, numTerms=1)
+        self.assertAlmostEqual(dtan(np.pi), 1.0)
         
 
 ##
@@ -161,13 +161,13 @@ class TestJacobian(unittest.TestCase):
         pass
 
     def testjacobian(self):
-        xdata = np.reshape(np.arange(0,1,0.1),(-1,1))
-        ydata = 1+2*np.exp(0.75*xdata)
-        fun = lambda c: (c[0]+c[1]*np.exp(c[2]*xdata) - ydata)**2
+        xdata = np.reshape(np.arange(0, 1, 0.1), (-1, 1))
+        ydata = 1 + 2 * np.exp(0.75 * xdata)
+        fun = lambda c: (c[0] + c[1] * np.exp(c[2] * xdata) - ydata) ** 2
         Jfun = nd.Jacobian(fun)
-        J = Jfun([1,2,0.75]) # should be numerically zero
+        J = Jfun([1, 2, 0.75]) # should be numerically zero
         for ji in J.ravel():
-            self.assertAlmostEqual(ji,0.0)
+            self.assertAlmostEqual(ji, 0.0)
 
 class TestGradient(unittest.TestCase):
 
@@ -178,12 +178,12 @@ class TestGradient(unittest.TestCase):
         pass
 
     def testgradient(self):
-        fun = lambda x: np.sum(x**2)
+        fun = lambda x: np.sum(x ** 2)
         dfun = nd.Gradient(fun)
-        d = dfun([1,2,3])
-        dtrue = [ 2.,  4.,  6.]
-        for (di,dit) in zip(d,dtrue):
-            self.assertAlmostEqual(di,dit)
+        d = dfun([1, 2, 3])
+        dtrue = [ 2., 4., 6.]
+        for (di, dit) in zip(d, dtrue):
+            self.assertAlmostEqual(di, dit)
 
 class TestHessian(unittest.TestCase):
 
@@ -196,12 +196,12 @@ class TestHessian(unittest.TestCase):
     def testhessian(self):
          #cos(x-y), at (0,0)
         cos = np.cos
-        fun = lambda xy : cos(xy[0]-xy[1])
+        fun = lambda xy : cos(xy[0] - xy[1])
         Hfun2 = nd.Hessian(fun)
         h2 = Hfun2([0, 0]) # h2 = [-1 1; 1 -1];
-        htrue = [-1.,  1., 1., -1.]
-        for (hi,hit) in zip(h2.ravel(),htrue):
-            self.assertAlmostEqual(hi,hit)
+        htrue = [-1., 1., 1., -1.]
+        for (hi, hit) in zip(h2.ravel(), htrue):
+            self.assertAlmostEqual(hi, hit)
         
 class TestHessdiag(unittest.TestCase):
 
@@ -212,12 +212,12 @@ class TestHessdiag(unittest.TestCase):
         pass
 
     def testhessdiag(self):
-        fun = lambda x : x[0] + x[1]**2 + x[2]**3
+        fun = lambda x : x[0] + x[1] ** 2 + x[2] ** 3
         Hfun = nd.Hessdiag(fun)
-        hd = Hfun([1,2,3])
-        htrue = [  0.,   2.,  18.]
-        for (hi,hit) in zip(hd,htrue):
-            self.assertAlmostEqual(hi,hit)
+        hd = Hfun([1, 2, 3])
+        htrue = [  0., 2., 18.]
+        for (hi, hit) in zip(hd, htrue):
+            self.assertAlmostEqual(hi, hit)
 
 
 class TestGlobalFunctions(unittest.TestCase):
@@ -231,14 +231,14 @@ class TestGlobalFunctions(unittest.TestCase):
     def testvec2mat(self):
         pass
     def testdea3(self):
-        Ei= np.zeros(3)
-        linfun = lambda k : np.linspace(0,np.pi/2.,2.**(k+5)+1)
+        Ei = np.zeros(3)
+        linfun = lambda k : np.linspace(0, np.pi / 2., 2. ** (k + 5) + 1)
         for k in np.arange(3): 
             x = linfun(k) 
-            Ei[k] = np.trapz(np.sin(x),x)
-        [En, err] = nd.dea3(Ei[0],Ei[1],Ei[2])
-        self.assertTrue(np.abs(En-1)<err)
-        self.assertAlmostEqual(En,1.0)
+            Ei[k] = np.trapz(np.sin(x), x)
+        [En, err] = nd.dea3(Ei[0], Ei[1], Ei[2])
+        self.assertTrue(np.abs(En - 1) < err)
+        self.assertAlmostEqual(En, 1.0)
 
 if __name__ == '__main__':
     unittest.main()