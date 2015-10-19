7eefbb652457b3fd8c79a315f5756e25e46ec0b4
pbrod <per.andreas.brodtkorb@gmail.com>
2015-10-19 18:42:19 +0200

    No change

--- docs/conf.py
+++ docs/conf.py
@@ -13,6 +13,7 @@ import os
 import inspect
 from sphinx import apidoc
 import sphinx_rtd_theme
+# import alabaster
 
 
 __location__ = os.path.join(os.getcwd(), os.path.dirname(
@@ -108,7 +109,7 @@ numpydoc_show_class_members = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.  'alabaster'
-html_theme = 'sphinx_rtd_theme'
+html_theme = 'sphinx_rtd_theme' # 'alabaster'  #
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
@@ -116,6 +117,7 @@ html_theme = 'sphinx_rtd_theme'
 # html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
+#html_theme_path = [alabaster.get_path()]
 html_theme_path = [sphinx_rtd_theme.get_html_theme_path()]
 
 # The name for this set of Sphinx documents.  If None, it defaults to
