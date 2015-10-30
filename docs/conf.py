5946ce52e9617019725974e3941bc70e5e4f999a
pbrod <per.andreas.brodtkorb@gmail.com>
2015-10-30 23:39:06 +0100

    updated version

--- docs/conf.py
+++ docs/conf.py
@@ -61,7 +61,7 @@ copyright = u"2009-2015, Per A Brodtkorb, John D'Errico"
 #
 
 # The full version, including alpha/beta/rc tags.
-release = '0.9.12'  # Is set by calling `setup.py docs`
+release = '0.9.13'  # Is set by calling `setup.py docs`
 # The short X.Y version.
 version = release.split('-', 1)[0]  # Is set by calling `setup.py docs`
 
