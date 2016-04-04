c430d8a7af156fa114fe9f7724372dd826c1ba05
pbrod <per.andreas.brodtkorb@gmail.com>
2016-04-04 14:17:04 +0200

    Moved rosen function into numdifftools.testing.py

---
+++ numdifftools/testing.py
@@ -0,0 +1,16 @@
+'''
+Created on Apr 4, 2016
+
+@author: pab
+'''
+import numpy as np
+
+
+def rosen(x):
+    """Rosenbrock function
+
+    This is a non-convex function used as a performance test problem for
+    optimization algorithms introduced by Howard H. Rosenbrock in 1960.[1]
+    """
+    x = np.atleast_1d(x)
+    return (1 - x[0])**2 + 105. * (x[1] - x[0]**2)**2
