ce0dd5de5a7d4d8cf682fc8b7e0850cc73a2d7d4
Per A Brodtkorb <per.andreas.brodtkorb@gmail.com>
2016-07-21 12:35:40 +0200

    simplified duplicated code

--- numdifftools/example_functions.py
+++ numdifftools/example_functions.py
@@ -21,7 +21,7 @@ function_names = ['cos', 'sin', 'tan',
 
 
 def darcsin(x):
-    return 1./sqrt(1-x**2)
+    return 1. / sqrt(1 - x**2)
 
 
 def ddarcsin(x):
@@ -30,7 +30,7 @@ def ddarcsin(x):
 
 def dddarcsin(x):
     y = darcsin(x)
-    return y**3 * (1 + 3 * x**2 * y**2)
+    return y**3 * (1 + 3 * (x * y) ** 2)
 
 
 def darccos(x):
@@ -48,19 +48,21 @@ def dddarccos(x):
 def derivative_arcsin(n):
     return (arcsin, darcsin, ddarcsin, dddarcsin, None)[min(n, 4)]
 
+
 def derivative_arccos(n):
     return (arccos, darccos, ddarccos, dddarccos, None)[min(n, 4)]
 
+
 def derivative_arctan(n):
     def darctan(x):
-        return 1./(1+x**2)
+        return 1. / (1 + x**2)
 
     def ddarctan(x):
-        return -2*x*darctan(x)**2
+        return -2 * x * darctan(x)**2
 
     def dddarctan(x):
         y = darctan(x)
-        return (8.0 * x**2 * y - 2.0) * y**2
+        return 2 * (4.0 * x**2 * y - 1.0) * y**2
 
     def ddddarctan(x):
         y = darctan(x)
@@ -68,7 +70,7 @@ def derivative_arctan(n):
     return (arctan, darctan, ddarctan, dddarctan, ddddarctan, None)[min(n, 5)]
 
 
-def  derivative_sin(n):
+def derivative_sin(n):
     def dcos(x):
         return -sin(x)
 
@@ -77,80 +79,99 @@ def  derivative_sin(n):
     return (sin, cos, dcos, ddcos)[n % 4]
 
 
-def  derivative_cos(n):
+def derivative_cos(n):
     return derivative_sin(n + 1)
+
+
+def derivative_tan(n):
+    def dtan(x):
+        return 1. / np.cos(x)**2
+
+    def ddtan(x):
+        return 2 * tan(x) / cos(x)**2
+
+    def dddtan(x):
+        y = tan(x)
+        return 2 * (y**2 + 1) * (3 * y**2 + 1)
+
+    def ddddtan(x):
+        y = tan(x)
+        return 8 * y * (y**2 + 1) * (3 * y**2 + 2)
+    return (tan, dtan, ddtan, dddtan, ddddtan, None)[min(n, 5)]
 
 
 def derivative_sinh(n):
     return (sinh, cosh)[n % 2]
 
-def derivative_tan(n):
-    def dtan(x):
-        return 1./np.cos(x)**2
-    def ddtan(x):
-        return 2*tan(x)/cos(x)**2
-    def dddtan(x):
-        y = tan(x)
-        return (4*(y**2 + 1)*y**2 + 2*(y**2 + 1)**2)
-    def ddddtan(x):
-        y = tan(x)
-        return 8*(y**2 + 1)*y**3 + 16*(y**2 + 1)**2*y
-    return (tan, dtan, ddtan, dddtan, ddddtan, None)[min(n, 5)]
 
 def derivative_cosh(n):
     return derivative_sinh(n + 1)
 
+
 def derivative_tanh(n):
     def dtanh(x):
         return 1. / cosh(x) ** 2
+
     def ddtanh(x):
         return -2 * sinh(x) / cosh(x) ** 3
+
     def dddtanh(x):
         y = cosh(x)
-        return 4*(tanh(x)/y)**2 - 2./y**4
+        return 4 * (tanh(x) / y)**2 - 2. / y**4
+
     def ddddtanh(x):
         y = tanh(x)
-        return (8*(y**2 - 1)*y**3 + 16*(y**2 - 1)**2*y)
+        return 8 * y * (y**2 - 1) * (3 * y**2 - 2)
     return (tanh, dtanh, ddtanh, dddtanh, ddddtanh, None)[min(n, 5)]
 
-def  derivative_arccosh(n):
+
+def _dddarc_h(x, y):
+    return (3 * (x * y) ** 2 - 1) * y ** 3
+
+
+def derivative_arccosh(n):
     def darccosh(x):
         return 1.0 / sqrt(x**2 - 1)
+
     def ddarccosh(x):
         return -x * darccosh(x)**3
+
     def dddarccosh(x):
-        y = darccosh(x)
-        return - y**3 + 3*x**2 * y**5
+        return _dddarc_h(x, darccosh(x))
     return (arccosh, darccosh, ddarccosh, dddarccosh, None)[min(n, 4)]
+
 
 def derivative_arcsinh(n):
     def darcsinh(x):
-        return 1.0 / sqrt(1+x**2)
+        return 1.0 / sqrt(1 + x**2)
+
     def ddarcsinh(x):
         return -x * darcsinh(x)**3
+
     def dddarcsinh(x):
-        y = darcsinh(x)
-        return - y**3 + 3*x**2 * y**5
+        return _dddarc_h(x, darcsinh(x))
     return (arcsinh, darcsinh, ddarcsinh, dddarcsinh, None)[min(n, 4)]
 
 
 def derivative_arctanh(n):
     def darctanh(x):
-        return 1.0 / (1-x**2)
+        return 1.0 / (1 - x**2)
+
     def ddarctanh(x):
-        return 2*x*darctanh(x)**2
+        return 2 * x * darctanh(x)**2
+
     def dddarctanh(x):
         y = darctanh(x)
-        return 2 * y**2 + 8 * x**2 * y**3
+        return 2 * y**2 * (1 + 4 * x**2 * y)
     return (arctanh, darctanh, ddarctanh, ddarctanh, None)[min(n, 4)]
 
 
 def derivative_exp(n):
-     return exp
+    return exp
 
 
 def derivative_expm1(n):
-     return (expm1, exp)[min(n, 1)]
+    return (expm1, exp)[min(n, 1)]
 
 
 def derivative_exp2(n):
@@ -161,64 +182,75 @@ def derivative_exp2(n):
 
 def derivative_square(n):
     def square(x):
-        return x*x
+        return x * x
+
     def dsquare(x):
         return 2 * x
+
     def ddsquare(x):
         return 2 * np.ones_like(x)
+
     def dddsquare(x):
         return np.zeros_like(x)
-    return (square, dsquare, ddsquare, dddsquare)[min(n,3)]
+    return (square, dsquare, ddsquare, dddsquare)[min(n, 3)]
 
 
 def derivative_log1p(n):
     def dlog1p(x):
-        return (-1)**(n+1)* special.gamma(n) / (1+x)**n
+        return (-1)**(n + 1) * special.gamma(n) / (1 + x)**n
     if n > 5:
         return None
     return (log1p, dlog1p)[min(n, 1)]
 
 
-def derivative_log2(n):
-    def dlog2(x):
-        return (-1)**(n+1)* special.gamma(n) / (x**n * log(2))
+def _derivative_loga(n, a=10):
+
     if n > 4:
         return None
-    return (log2, dlog2)[min(n, 1)]
+    dlog = derivative_log(n)
+
+    def dlog_a(x):
+        return dlog(x) / log(a)
+    return dlog_a
+
+
+def derivative_log2(n):
+    return _derivative_loga(n, a=2)
+
 
 def derivative_log10(n):
-    def dlog10(x):
-        return (-1)**(n+1)* special.gamma(n) / (x**n * log(10))
-    if n > 4:
-        return None
-    return (log10, dlog10)[min(n, 1)]
+    return _derivative_loga(n, a=10)
+
 
 def derivative_log(n):
-    def dlog(x):
-        return (-1)**(n+1)* special.gamma(n) / x**n
     if n > 4:
         return None
+
+    def dlog(x):
+        return (-1)**(n + 1) * special.gamma(n) / x**n
     return (log, dlog)[min(n, 1)]
 
 
 def derivative_sqrt(n):
-    fact = 0.5*(-1)**(n+1)
-    for k in np.arange(.5, n-1):
+    fact = 0.5 * (-1)**(n + 1)
+    for k in np.arange(.5, n - 1):
         fact *= k
 
     def dsqrt(x):
         sx = sqrt(x)
-        return fact / sx**(2*n-1)
-    if n>5:
+        return fact / sx**(2 * n - 1)
+    if n > 5:
         return None
     return (sqrt, dsqrt)[min(n, 1)]
 
+
 def derivative_inv(n):
     def inv(x):
-        return 1./x
+        return 1. / x
+
     def dinv(x):
-        return (-1)**n * special.gamma(n) / x**(n+1)
-    return (inv, dinv)[min(n,1)]
+        return (-1)**n * special.gamma(n) / x**(n + 1)
+    return (inv, dinv)[min(n, 1)]
 
 
 def get_function(fun_name, n=1):
