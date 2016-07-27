504f1e8ea4d23711728b07d643f007d20094e0c7
Per A Brodtkorb <per.andreas.brodtkorb@gmail.com>
2016-07-27 20:10:16 +0200

    Expanded the tests + added test_docstrings to testing.py

--- numdifftools/testing.py
+++ numdifftools/testing.py
@@ -3,6 +3,7 @@ Created on Apr 4, 2016
 
 @author: pab
 '''
+import inspect
 import numpy as np
 
 
@@ -14,3 +15,10 @@ def rosen(x):
     """
     x = np.atleast_1d(x)
     return (1 - x[0])**2 + 105. * (x[1] - x[0]**2)**2
+
+
+def test_docstrings():
+    # np.set_printoptions(precision=6)
+    import doctest
+    print('Testing docstrings in %s' % inspect.stack()[1][1])
+    doctest.testmod(optionflags=doctest.NORMALIZE_WHITESPACE|doctest.ELLIPSIS)
--- numdifftools/tests/test_example_functions.py
+++ numdifftools/tests/test_example_functions.py
@@ -1,6 +1,7 @@
 import unittest
 import numpy as np
 import numdifftools.core as nd
+import numdifftools.nd_algopy as nda
 from numpy.testing import assert_array_almost_equal
 from numdifftools.example_functions import function_names, get_function
 
@@ -9,20 +10,28 @@ class TestExampleFunctions(unittest.TestCase):
     @staticmethod
     def test_high_order_derivative():
         x = 0.5
-        methods = ['complex', 'central', 'forward', 'backward']
-        for name in function_names:
-            for n in range(2, 11):
-                f, true_df = get_function(name, n=n)
-                if true_df is None:
-                    continue
-                for method in methods:
-                    if n > 7 and method not in ['complex']:
-                        continue
-                    val = nd.Derivative(f, method=method, n=n)(x)
-                    print(name, method, n)
-                    tval = true_df(x)
-                    assert_array_almost_equal(val, tval, decimal=2)
+        min_dm = dict(complex=2, forward=2, backward=2, central=4)
+        methods = [ 'complex', 'central',  'backward', 'forward']
 
+        for i, Derivative in enumerate([nd.Derivative, nda.Derivative]):
+            for name in function_names:
+                if i>0 and name in ['arcsinh', 'exp2']:
+                    continue
+                for n in range(2, 11):
+                    f, true_df = get_function(name, n=n)
+                    if true_df is None:
+                        continue
+                    for method in methods[3*i:]:
+                        if i==0 and n > 7 and method not in ['complex']:
+                            continue
+                        df = Derivative(f, method=method, n=n, full_output=True)
+                        val, info = df(x)
+                        dm = max(int(-np.log10(info.error_estimate + 1e-16))-1,
+                                 min_dm.get(method, 4))
+                        print(i, name, method, n, dm)
+                        tval = true_df(x)
+                        assert_array_almost_equal(val, tval, decimal=dm)
+        # assert(False)
 
 if __name__ == '__main__':
     unittest.main()
