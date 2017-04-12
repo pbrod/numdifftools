8557f10526a67e067977074540165d37b7f21595
dhirschf <david.hirschfeld@stanwell.com>
2017-04-13 09:11:03 +1000

    Remove deprecated testing method

--- conda_recipe/run_test.py
+++ conda_recipe/run_test.py
@@ -1,3 +1,10 @@
-import numdifftools
+import os
 
-numdifftools.test()
+import numdifftools
+import pytest
+
+
+path = os.path.join(numdifftools.__path__[0], 'tests')
+os.chdir(path)
+
+pytest.main()
--- numdifftools/__init__.py
+++ numdifftools/__init__.py
@@ -1,13 +1,4 @@
-import pkg_resources
 from .info import __doc__
 from .core import *
 from . import extrapolation, limits, step_generators
 
-from numpy.testing import Tester
-try:
-    __version__ = pkg_resources.get_distribution(__name__).version
-except pkg_resources.DistributionNotFound:
-    __version__ = 'unknown'
-
-
-test = Tester(raise_warnings="release").test
