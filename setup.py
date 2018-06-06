c1fcae904e971867a014923e79d2c6621a79dd37
Per A Brodtkorb <per.andreas.brodtkorb@gmail.com>
2018-06-06 13:44:12 +0200

    Attempt to fix installation for python 2.7 See issue #37

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ PyPi upload:
 """
 
 import sys
-from setuptools import setup
+from setuptools import setup, find_packages
 
 
 def print_version():
@@ -60,7 +60,8 @@ def print_version():
 def setup_package():
     needs_sphinx = {'build_sphinx', 'upload_docs'}.intersection(sys.argv)
     sphinx = ['sphinx', 'numpydoc'] if needs_sphinx else []
-    setup(setup_requires=['six', 'pyscaffold==2.5.11'] + sphinx,
+    setup(setup_requires=['pyscaffold==2.5.11'] + sphinx,
+          packages=find_packages(where='./src'),
           # tests_require=['pytest_cov', 'pytest', 'hypothesis', 'matplotlib'],
           use_pyscaffold=True)
     print_version()
