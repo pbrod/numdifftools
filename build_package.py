975881d557b4699e115228b1b67627f7bd5a13f6
Per A Brodtkorb <per.andreas.brodtkorb@gmail.com>
2020-09-23 22:15:50 +0200

    Updated build_package.py

--- build_package.py
+++ build_package.py
@@ -1,11 +1,11 @@
 """
-Script to build the numdifftools package.
+Script to build the package.
 
-The script remove the previous built binaries and generated documentation
+The script removes the previous built binaries and generated documentation
 before it generate the documentation and build the binaries and finally
 check the built binaries.
 
-It assumes that the numdifftools library is installed in so called develop mode.
+It assumes that the library is installed in so called develop mode.
 
 Created on 7. des. 2018
 
@@ -26,38 +26,14 @@ PACKAGE_NAME = 'numdifftools'
 
 
 def remove_previous_build():
+    """Removes ./dist, ./build, ./docs/_build, and ./src/{}.egg-info directories.""".format(PACKAGE_NAME)
     egginfo_path = os.path.join('src', '{}.egg-info'.format(PACKAGE_NAME))
-    docs_folder = os.path.join(ROOT, 'docs', '_build')
+    docs_folder = os.path.join('docs', '_build')
 
     for dirname in ['dist', 'build', egginfo_path, docs_folder]:
         path = os.path.join(ROOT, dirname)
         if os.path.exists(path) and os.path.isdir(path):
             shutil.rmtree(path)
-
-
-@click.command(context_settings=dict(help_option_names=['-h', '--help']))
-@click.argument("version")
-def build_main(version):
-    """Build and update {} version, documentation and package.
-
-    The script remove the previous built binaries and generated documentation
-    before it generate the documentation and build the binaries and finally
-    check the built binaries.
-    """.format(PACKAGE_NAME)
-    remove_previous_build()
-    set_package(version)
-    update_readme()
-
-    for cmd in ['docs', 'sdist', 'bdist_wheel', 'egg_info']:
-        try:
-            subprocess.call(["python", "setup.py", cmd])
-        except Exception as error:  # subprocess.CalledProcessError:
-            print('{}: {}'.format(cmd, str(error)))
-    try:
-        subprocess.call(["twine", "check", "dist/*"])
-    except Exception as error:  # subprocess.CalledProcessError:
-        print("Twine: ", str(error))
-
 
 def update_readme():
     readme_txt = INFO_TXT.replace(
@@ -88,6 +64,54 @@ def set_package(version):
 
         with open(filename, "w") as fid:
             fid.write(new_text)
+
+
+class ChangeDir:
+    """Context manager for changing the current working directory"""
+    def __init__(self, new_path):
+        self.new_path = new_path
+
+    def __enter__(self):
+        self.saved_path = os.getcwd()
+        os.chdir(self.new_path)
+
+    def __exit__(self, etype, value, traceback):
+        os.chdir(self.saved_path)
+
+
+def call_subprocess(cmd_opts):
+    """Safe call to subprocess"""
+    print("\n\n***********************************************")
+    print("Running {}".format(' '.join(cmd_opts))
+    try:
+        subprocess.call(cmd_opts)
+    except Exception as error:  # subprocess.CalledProcessError:
+        print(str(error))
+    print("***********************************************\n")
+
+
+
+@click.command(context_settings=dict(help_option_names=['-h', '--help']))
+@click.argument("version")
+def build_main(version):
+    """Build and update {} version, documentation and package.
+
+    The script remove the previous built binaries and generated documentation
+    before it generate the documentation and build the binaries
+    and finally check the built binaries.
+    """.format(PACKAGE_NAME)
+    remove_previous_build()
+    set_package(version)
+    update_readme()
+
+    for cmd in ['docs', 'latexpdf', 'sdist', 'bdist_wheel', 'egg_info']:
+        if cmd == 'latexpdf':
+            with ChangeDir('./docs'):
+                call_subprocess(["make.bat", cmd])
+        else:
+            call_subprocess(["python", "setup.py", cmd])
+
+    call_subprocess(["twine", "check", "dist/*"])
 
 
 if __name__ == "__main__":
