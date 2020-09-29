Fixes issue #49 : Dimension of Jacobian of vector valued function (length n) with scalar input should be n X 1
Added license.py

src/numdifftools/core.py
@@ -658,16 +658,18 @@ class Jacobian(Derivative):
         return tuple(original_shape)
 
     def _vstack(self, sequence, steps):
-        original_shape = list(np.shape(np.atleast_1d(sequence[0].squeeze())))
+        original_shape = list(np.shape(np.atleast_1d(sequence[0])))
         ndim = len(original_shape)
-        axes = [0, 1, 2][:ndim]
-        axes[:2] = axes[1::-1]
-        original_shape[:2] = original_shape[1::-1]
+        if sum(original_shape) == ndim:
+            f_del = np.vstack(sequence)
+            h = np.vstack(steps)
+        else:
+            axes = [0, 1, 2][:ndim]
+            axes[:2] = axes[1::-1]
+            original_shape[:2] = original_shape[1::-1]
 
-        f_del = np.vstack([np.atleast_1d(r.squeeze()).transpose(axes).ravel()
-                           for r in sequence])
-        h = np.vstack([np.atleast_1d(r.squeeze()).transpose(axes).ravel()
-                       for r in steps])
+            f_del = np.vstack([np.atleast_1d(r).transpose(axes).ravel() for r in sequence])
+            h = np.vstack([np.atleast_1d(r).transpose(axes).ravel() for r in steps])
         _assert(f_del.size == h.size, 'fun did not return data of correct '
                 'size (it must be vectorized)')
         return f_del, h, self._atleast_2d(original_shape, ndim)

src/numdifftools/nd_statsmodels.py
@@ -55,7 +55,7 @@ def approx_fprime(x, f, epsilon=None, args=(), kwargs=None, centered=True):
 
     """
     kwargs = {} if kwargs is None else kwargs
-    x = np.atleast_1d(x).ravel()
+    x = np.atleast_1d(x)  # .ravel()
     n = len(x)
     f0 = f(*(x,) + args, **kwargs)
     dim = np.atleast_1d(f0).shape  # it could be a scalar
@@ -83,7 +83,7 @@ def approx_fprime(x, f, epsilon=None, args=(), kwargs=None, centered=True):
 
 
 def _approx_fprime_backward(x, f, epsilon=None, args=(), kwargs=None):
-    x = np.atleast_1d(x).ravel()
+    x = np.atleast_1d(x) # .ravel()
     n = len(x)
     epsilon = - np.abs(_get_epsilon(x, 2, epsilon, n))
     return approx_fprime(x, f, epsilon, args, kwargs, centered=False)
@@ -123,7 +123,7 @@ def approx_fprime_cs(x, f, epsilon=None, args=(), kwargs=None):
     # May 04 2010 thread "Improvement of performance"
     # http://mail.scipy.org/pipermail/numpy-discussion/2010-May/050250.html
     kwargs = {} if kwargs is None else kwargs
-    x = np.atleast_1d(x).ravel()
+    x = np.atleast_1d(x) # .ravel()
     n = len(x)
     epsilon = _get_epsilon(x, 1, epsilon, n)
     increments = np.identity(n) * 1j * epsilon

src/numdifftools/tests/test_numdifftools.py
@@ -298,11 +298,11 @@ class TestJacobian(object):
         def fun(x):
             return np.array([x, x ** 2, x ** 3])
 
-        truth = np.array([[1., 2 * val, 3 * val ** 2]])
+        truth = np.array([[[1.]], [[2 * val]], [[3 * val ** 2]]])
         for method in ['multicomplex', 'complex', 'central', 'forward', 'backward']:
             j0, info = nd.Jacobian(fun, method=method, full_output=True)(val)
             if method != "multicomplex":
-                j00 = nds.Jacobian(fun, method=method)(val).T
+                j00 = nds.Jacobian(fun, method=method)(val)
                 error = np.abs(j00 - truth)
                 note('statsmodel: method={}, error={}'.format(method, error))
                 assert_allclose(j00, truth, rtol=1e-3, atol=1e-6)
@@ -388,8 +388,8 @@ class TestJacobian(object):
                                 [75., 108., 147., 192.]]])
         v0 = nds.approx_fprime(x, fun)
         val = jaca(x)
-        assert_allclose(v0, tval)
         assert_allclose(val, tval)
+        assert_allclose(v0, tval)
 
     @staticmethod
     def test_issue_25():
@@ -441,6 +441,7 @@ class TestJacobian(object):
     @staticmethod
     @pytest.mark.slow
     def test_issue_27b():
+        """Test for memory-error"""
         n = 1000
         x = np.ones(n)
         assert_allclose(nd.Jacobian(lambda x: x ** 2, method='complex')(x),